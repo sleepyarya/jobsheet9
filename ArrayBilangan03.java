7faf1cc4d321b57cdf8f2c77f8657d5707e62623
sleepyarya <jjill2479@gmail.com>
2024-10-31 13:05:52 +0700

    array mod

--- ArrayBilangan03.java
+++ ArrayBilangan03.java
@@ -1,15 +1,22 @@
 package jobsheet9;
-public class ArrayBilangan03{
+
+public class ArrayBilangan03 {
     public static void main(String[] args) {
-        int[] bil = new int [4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
-        
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        // int[] bil = new int [4];
+        // bil[0] = 5;
+        // bil[1] = 13;
+        // bil[2] = -7;
+        // bil[3] = 17;
+
+        int[] bil = { 5, 13, -7, 17 };
+
+        // System.out.println(bil[0]);
+        // System.out.println(bil[1]);
+        // System.out.println(bil[2]);
+        // System.out.println(bil[3]);
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
